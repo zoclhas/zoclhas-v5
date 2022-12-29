be00ece83e5297d408193920b973aef939b222d2
Zoclhas <zoclhas@gmail.com>
2022-12-30 01:10:08 +0400

    Completed works section

--- components/sections/Work.js
+++ components/sections/Work.js
@@ -1,3 +1,107 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+
+import { works } from "./work/works";
+import workcss from "../../styles/work.module.css";
+
 export default function About() {
-    return <section id="work"></section>;
+    var projectCards;
+    var showButton;
+    var hideButton;
+    if (typeof document !== "undefined") {
+        projectCards = document.querySelector("[data-more]");
+        showButton = document.getElementById("show-more-btn");
+        hideButton = document.getElementById("show-less-btn");
+    }
+    hideButton.style.display = "none";
+
+    const showProjectCards = () => {
+        projectCards.setAttribute("data-more", "show");
+        showButton.style.display = "none";
+        hideButton.style.display = "block";
+    };
+
+    const hideProjectCards = () => {
+        projectCards.setAttribute("data-more", "hide");
+        showButton.style.display = "block";
+        hideButton.style.display = "none";
+    };
+
+    return (
+        <section id="work">
+            <div className="card">
+                <div className="card-header">
+                    <h1>Work</h1>
+                </div>
+            </div>
+            <div className="spacer"></div>
+            <div className={workcss["project-cards"]} data-more="hide">
+                {works.map((work) => (
+                    <div className="card">
+                        <div className={workcss["card-split"]}>
+                            <img
+                                src={`/${work.coverImage}`}
+                                alt={`${work.title}'s image`}
+                            />
+                            <div>
+                                <div
+                                    className="card-header"
+                                    style={{
+                                        display: "flex",
+                                        justifyContent: "space-between",
+                                        alignItems: "center",
+                                    }}
+                                >
+                                    <a
+                                        href={work.link}
+                                        target="_blank"
+                                        rel="noreferrer"
+                                        className="no-underline"
+                                    >
+                                        <h2>{work.title}</h2>
+                                        {work.isExternal && (
+                                            <FontAwesomeIcon
+                                                icon={faArrowUpRightFromSquare}
+                                            />
+                                        )}
+                                    </a>
+                                    {work.github && (
+                                        <h2>
+                                            <a
+                                                href={work.github}
+                                                target="_blank"
+                                                rel="noreferrer"
+                                                className="no-underline"
+                                            >
+                                                <FontAwesomeIcon
+                                                    icon={faGithub}
+                                                />
+                                            </a>
+                                        </h2>
+                                    )}
+                                </div>
+                                <div
+                                    className="card-body"
+                                    style={{
+                                        height: "calc(100% - 120px)",
+                                        display: "flex",
+                                        alignItems: "center",
+                                    }}
+                                >
+                                    {work.description}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+                <button id="show-more-btn" onClick={showProjectCards}>
+                    Show More
+                </button>
+                <button id="show-less-btn" onClick={hideProjectCards}>
+                    Show Less
+                </button>
+            </div>
+        </section>
+    );
 }
---
+++ components/sections/work/works.js
@@ -0,0 +1,47 @@
+export const works = [
+    {
+        coverImage: "zocs-space.png",
+        title: "zocs.space",
+        link: "https://zocs.space",
+        description:
+            "Personal blogging site for all sorts of randomg thoughts, code, etc. Made using NextJS, NextUI and with `markdown-it` for blogs.",
+        github: "https://github.com/zoclhas/blog",
+        isExternal: true,
+    },
+    {
+        coverImage: "zaura-net.png",
+        title: "zaura.net",
+        link: "https://zaura.net",
+        description:
+            "Zaura is a professional studio creating high-quality resourepacks for Minecraft. It showcases pricing, downloads, updates, gallery. Made using NextJS & NextUI and Django serves as the backend.",
+        github: null,
+        isExternal: true,
+    },
+    {
+        coverImage: "Amulet.webp",
+        title: "Amulet",
+        link: "https://zaura.net/amulet",
+        description:
+            "Amulet is a high-res stylized POM/PBR resourcepack for Minecraft. Generated procedurally using Substance Designer and compiled using PixelGraph.",
+        github: null,
+        isExternal: true,
+    },
+    {
+        coverImage: "zochy-bio.png",
+        title: "zochy.xyz",
+        link: "https://zochy.xyz",
+        description:
+            "A small bio-type of site, with external links. That but just a bit more customised and has a better UI.",
+        github: "https://github.com/zoclhas/zochy-bio",
+        isExternal: true,
+    },
+    {
+        coverImage: "shivamzter-com.png",
+        title: "shivamzter.com",
+        link: "https://shivamzter.com",
+        description:
+            "Client project for showcasing their resourecepacks. Gallery and Roadmap pages are made using ReactJS and Django as backend.",
+        github: null,
+        isExternal: true,
+    },
+];
