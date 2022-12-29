06420f66282d1c66fff8b6cb32e11757f8c9b715
Zoclhas <zoclhas@gmail.com>
2022-12-29 20:33:54 +0400

    Completed profile side column

---
+++ components/sections/Sections.js
@@ -0,0 +1,13 @@
+import About from "./About";
+import Work from "./Work";
+import Contact from "./Contact";
+
+export default function Sections() {
+    return (
+        <div>
+            <About />
+            <Work />
+            <Contact />
+        </div>
+    );
+}
